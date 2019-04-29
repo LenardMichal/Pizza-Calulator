cc3447e5ce33cc65757604c5b754f6e73a590ded
Michal <lenard.michal@yahoo.com>
2019-04-29 13:20:20 +0200

    Initial dist subtree commit

---
+++ precache-manifest.aab42d19aca0a95d3471375ebd8abb4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<pizza-calculator>/robots.txt"
+  },
+  {
+    "revision": "d14537b56808216cc986",
+    "url": "/<pizza-calculator>/js/chunk-vendors.8a5864eb.js"
+  },
+  {
+    "revision": "73e6d50544062aacdcd8",
+    "url": "/<pizza-calculator>/js/app.7eab1cca.js"
+  },
+  {
+    "revision": "4ba6a3f249154ca9bf13121dce1e0165",
+    "url": "/<pizza-calculator>/index.html"
+  },
+  {
+    "revision": "73e6d50544062aacdcd8",
+    "url": "/<pizza-calculator>/css/app.3249b4ad.css"
+  }
+];
